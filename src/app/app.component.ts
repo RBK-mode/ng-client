a32b3dcea19fd21d822b34d40e800c0748fa7ca2
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-16 09:48:35 +0100

    registered components in app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-learn';
+  courses = {
+    id: 1,
+    name: 'rbk',
+    isFavorite: false
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminItemsComponent } from './admin/admin-items/admin-items.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ItemsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    MyOrdersComponent,
+    AdminItemsComponent,
+    AdminOrdersComponent,
+    OrderSuccessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'items', component: ItemsComponent},
+      {path: 'shopping-cart', component: ShoppingCartComponent},
+      {path: 'check-out', component: CheckOutComponent},
+      {path: 'order-success', component: OrderSuccessComponent},
+      {path: 'my/orders', component: MyOrdersComponent},
+      {path: 'admin/items', component: AdminItemsComponent},
+      {path: 'admin/orders', component: AdminOrdersComponent}
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
