cd324b6c947cf98450382210edfb5779ba897f2a
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-16 19:42:50 +0100

    saves token and current user in local storage

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+
 import {environment} from '../../environments/environment';
 import {map} from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private user;
 
   constructor(private http: HttpClient) { }
 
@@ -19,13 +23,15 @@ export class AuthService {
     console.log(userData);
     return this.http.post(`${environment.apiUrl}/user/login`, userData).pipe(
       map((res: any) => {
-        return this.saveToken(res.token);
+        this.user = res.user;
+        return this.saveTokenAndCurrentUser(res.token);
       })
     );
   }
 
-  private saveToken(token: string): string {
-    localStorage.setItem('token', token);
-    return token;
+  private saveTokenAndCurrentUser(token: string): string {
+     localStorage.setItem('token', token);
+     localStorage.setItem('currentUser', JSON.stringify(this.user));
+     return token;
   }
 }
